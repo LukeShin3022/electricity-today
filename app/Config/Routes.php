c8dc4170be19f7a58d241e047de774ea3a888d1a
Luke Shin <jwshin3022@gmail.com>
2023-12-14 21:54:26 -0800

    Electricity Proj: Reproduce Login page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// $route['login'] = 'login';
+$routes->get('login','Pages::login');
+// $routes->view('login', 'login');
